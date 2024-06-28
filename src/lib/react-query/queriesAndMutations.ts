d60b10bfc1ffea9ceb5b5273d8bbbb4b2e92792a
Ali Shibli <alishibli372@gmail.com>
2024-06-28 08:13:51 +0300

    home-feed

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,11 +1,7 @@
-import {
-    useQuery,
-    useMutation,
-    useQueryClient,
-    useInfiniteQuery,
-} from '@tanstack/react-query'
-import { createPost, createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
+import { useQuery, useMutation, useQueryClient, useInfiniteQuery } from '@tanstack/react-query'
+import { createPost, createUserAccount, getRecentPosts, signInAccount, signOutAccount } from '../appwrite/api'
 import { INewPost, INewUser } from '@/types'
+import { QUERY_KEYS } from './queryKeys'
 
 export const useCreateUserAccount = () => {
     return useMutation({
@@ -13,10 +9,16 @@ export const useCreateUserAccount = () => {
     })
 }
 export const useCreatePost = () => {
+    const queryClient = useQueryClient();
     return useMutation({
-      mutationFn: (post: INewPost) => createPost(post),
+        mutationFn: (post: INewPost) => createPost(post),
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
+            })
+        },
     });
-  };
+};
 export const useSignInAccount = () => {
     return useMutation({
         mutationFn: (user: { email: string; password: string }) => signInAccount(user)
@@ -25,5 +27,11 @@ export const useSignInAccount = () => {
 export const useSignOutAccount = () => {
     return useMutation({
         mutationFn: signOutAccount
+    })
+}
+export const useGetRecentPost = () => {
+    return useQuery({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        queryFn: getRecentPosts,
     })
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,27 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 export const convertFileToUrl = (file: File) => URL.createObjectURL(file);
+
+export function timeAgo(dateString: string): string {
+  const date: Date = new Date(dateString);
+  const now: Date = new Date();
+  const diffInSeconds: number = Math.floor((now.getTime() - date.getTime()) / 1000);
+
+  const units: { name: string; seconds: number }[] = [
+    { name: "year", seconds: 31536000 },
+    { name: "month", seconds: 2592000 },
+    { name: "week", seconds: 604800 },
+    { name: "day", seconds: 86400 },
+    { name: "hour", seconds: 3600 },
+    { name: "minute", seconds: 60 },
+    { name: "second", seconds: 1 },
+  ];
+
+  for (const unit of units) {
+    const interval: number = Math.floor(diffInSeconds / unit.seconds);
+    if (interval >= 1) {
+      return `${interval} ${unit.name}${interval !== 1 ? "s" : ""} ago`;
+    }
+  }
+  return "just now";
+}
